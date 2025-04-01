2741f7f5a70a554c1121eb3c38e89987c861e4ec
JS <35227625+just-ajs@users.noreply.github.com>
2025-04-01 17:53:01 -0400

    add placeholder for remote server

--- src/scripts/compute.js
+++ src/scripts/compute.js
@@ -3,8 +3,12 @@ import RhinoCompute from "compute-rhino3d"
 import { store } from "@/stores/storeSingleton"
 
 // Local RhinoCompute endpoint  
-RhinoCompute.url = "http://localhost:6500/" //Rhino 7
-//RhinoCompute.url = "http://localhost:5000/" //Rhino 8
+RhinoCompute.url = "http://localhost:8081/" //Rhino 7
+//RhinoCompute.url = "http://localhost:6500/" //Rhino 8
+
+// TODO: modify to use remote server
+//RhinoCompute.url = "url"
+//RhinoCompute.apiKey = "api key"
 
 let rhino, doc, res
 
